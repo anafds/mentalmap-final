f11fa320145807375b4035050840d419abd8e969
Carol Frescurato <anacarolinafrescuratodasilva@MacBook-Air-de-Ana.local>
2025-01-20 21:11:46 -0300

    Corrigido deploy no Render sem apt

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Atualizando pacotes..."
-apt-get update
-
-echo "Instalando dependências para o Chrome..."
-apt-get install -y wget gnupg
-apt-get install -y libx11-xcb1 libxcomposite1 libxrandr2 libxi6 libatk1.0-0 libgtk-3-0 libnss3 libxdamage1 libxext6 libxshmfence1
-apt-get install -y libgbm-dev
